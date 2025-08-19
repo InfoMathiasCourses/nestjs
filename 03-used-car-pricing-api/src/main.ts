7be259973c8d7c9f899170adfc8159ae8d985812
InfoMathias <Mathias2820@gmail.com>
2025-08-18 23:30:24 -0400

    authentication with session management, user singleton interceptor and route auth guard

--- 03-used-car-pricing-api/src/main.ts
+++ 03-used-car-pricing-api/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session'); // import like this due to config mismatch with nest
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['asdiasj']
+  }))
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist:true,
